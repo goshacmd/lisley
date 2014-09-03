4638008cd6941eab667e7a2173b846f5636f7ee3
Gosha Arinich <me@goshakkk.name>
2014-09-03 20:12:47 +0300

    add repl

--- src/Main.hs
+++ src/Main.hs
@@ -1,14 +1,35 @@
+import Lisley.Types
 import Lisley.Parser
 import Lisley.Eval
 import Text.Parsec
+import Control.Monad
+import System.IO
 import System.Environment
+
+trapError action = catchError action (return . show)
+
+extractValue :: ThrowsError a -> a
+extractValue (Right val) = val
 
 run :: String -> String
 run input = case (readExpr input) >>= eval of
   Left err  -> "Got an error: " ++ show err
   Right val -> show val
 
+prompt :: String -> IO String
+prompt p = putStr p >> hFlush stdout >> getLine
+
+evalAndPrint :: String -> IO ()
+evalAndPrint e = (return . extractValue . trapError . liftM show $ readExpr e >>= eval) >>= putStrLn
+
+repl :: IO ()
+repl = prompt "> " >>= evalAndPrint >> repl
+
 main :: IO ()
 main = do
-  args <- getArgs
-  putStrLn (run (args !! 0))
+  (com:args) <- getArgs
+
+  case com of
+    "eval"    -> putStrLn (run (args !! 0))
+    "repl"    -> repl
+    otherwise -> putStrLn $ "Unknown command: " ++ com
